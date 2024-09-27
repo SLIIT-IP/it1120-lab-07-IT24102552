44f66906b3aaaf0882a0c42b241b56fd2b0c98ec
IT24102552 <IT24102552@my.sliit.lk>
2024-09-27 07:50:45 +0530

    Lab7Q2C

---
+++ Q2/IT24102552Lab7Q2C.java
@@ -0,0 +1,14 @@
+public class IT24102552Lab7Q2C
+{
+public static void main(String[]args)
+{
+for(int i=5;i>=1;i=i-1)
+{
+for(int j=1;j<=i;j=j+1)
+{
+System.out.print(i);
+}
+System.out.println();
+}
+}
+}
